e1c3ac3d513abd309a55d3665d5abc5b24e3c35e
sophocles99 <22369144+sophocles99@users.noreply.github.com>
2024-02-05 17:16:30 +0000

    reorganise folders

---
+++ tutorials/weather-app/src/app/page.tsx
@@ -0,0 +1,8 @@
+import NavBar from "@/components/NavBar";
+import Image from "next/image";
+
+export default function Home() {
+  return <div className="flex flex-col gap-4 bg-gray-100 min-h-screen">
+    <NavBar />
+  </div>;
+}
---
+++ tutorials/weather-app/src/components/NavBar.tsx
@@ -0,0 +1,23 @@
+/** @format */
+
+import React from "react";
+import { MdWbSunny } from "react-icons/md";
+import { MdMyLocation } from "react-icons/md";
+
+type Props = {};
+
+export default function NavBar({}: Props) {
+  return (
+    <nav className="shadow-sm sticky top-0 left-0 z-50 bg-white">
+      <div className="h-[80px] w-full flex justify-between items-center max-w-7xl px-3 mx-auto">
+        <div className="flex items-center justify-center gap-2">
+          <h2 className="text-gray-500 text-3xl">Weather</h2>
+          <MdWbSunny className="text-3xl mt-1 text-yellow-300"/>
+        </div>
+        <section className="flex gap-2 items-center">
+        <MdMyLocation />
+        </section>
+      </div>
+    </nav>
+  );
+}
